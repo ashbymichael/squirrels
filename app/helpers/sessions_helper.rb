6cceb59214db3a91c608fece332959919338ebd5
Michael Ashby <ashby.michael@gmail.com>
2017-02-17 21:20:26 -0800

    clean up sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,11 +5,10 @@ module SessionsHelper
 
   def log_out
     session.delete(:user_id)
-    @current_user = nil
   end
 
   def current_user
-    @current_user || User.find_by(id: session[:user_id])
+    User.find_by_id(session[:user_id])
   end
 
   def logged_in?
